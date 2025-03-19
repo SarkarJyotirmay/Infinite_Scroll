553ccd4455d8223cdb0f76bb2dcab210a7c894e4
SarkarJyotirmay <jsarkar0201@gmail.com>
2025-03-19 22:06:27 +0530

    fixed bug not hitting API while scroll, updated condition of scroll event on window

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const API_ACCESS_KEY = "6qPx-WOPnlVxAD3cHUis2rVxAwPhsZJUafwFLHAZ_-w";
+const API_ACCESS_KEY2 = "pPf0b6TnHCt3ROnc3nzNo1RpP42QQl_Cqbt68X7Hq9c";
+
 const basePath = `https://api.unsplash.com/photos/random`;
 
-let loaded;
+
+let loaded = false; // to check if the images from API has been loaded atleast once or not
+
 const imageContainer = document.querySelector(".image-container");
 const loader = document.querySelector(".lds-ripple");
 const mode = document.querySelector(".mode");
 const round = document.querySelector(".round");
 
 
-
+// calls getData and displayData function to fetch and show data(Images)
 async function getPhotos(){
-   let data = await getData(`${basePath}?count=30&client_id=${API_ACCESS_KEY}`);
+   let data = await getData(`${basePath}?count=30&client_id=${API_ACCESS_KEY2}`);
+   console.log("get photos se a raha hai");
    displayPhotos(data);
-
 }
 
+// 
  function displayPhotos(arr){
     loader.style.display = "block";
     const fragment = document.createDocumentFragment();
@@ -51,12 +56,14 @@ async function getData(url){
    }
 }
 
+// implimenting throttling on the scroll
 window.addEventListener("scroll", (e)=>{
-    if(window.scroll.Y + window.innerHeight >= document.body.offsetHeight && loaded){
+    if(Math.ceil(window.scrollY) + window.innerHeight >= document.body.offsetHeight && loaded){
         getPhotos();
     }
 })
 
+//! Start of the code execution on page 
 getPhotos();
 
 // mode
